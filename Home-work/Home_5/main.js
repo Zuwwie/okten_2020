431894300bc5a84a4e3241534e344e95c775265c
Nazar Havronskyi <nazar.gavronsky.mail@gmail.com>
2020-10-28 12:26:16 +0200

    first_part

---
+++ Home-work/Home_5/main.js
@@ -0,0 +1,156 @@
+// - присвоить каждому из следующих значений свою переменную: 'hello','owu','com', 'ua', 1, 10, -999, 123, 3.14, 2.7, 16, true, false Вывести каждую при помощи console.log , alert, document.write
+
+// let a1 = `hello`;
+// let a2 = `owu`;
+// let a3 = `com`;
+// let a4 = `ua`;
+// let a5 = 1;
+// let a6 = 10;
+// let a7 = -999;
+// let a8 = 123;
+// let a9 = 3.14;
+// let a10 = 2.7;
+// let a11 = 16;
+// let a12 = true;
+// let a13 = false;
+
+// console.log(a1);
+// console.log(a2);
+// console.log(a3);
+// console.log(a4);
+// console.log(a5);
+// console.log(a6);
+// console.log(a7);
+// console.log(a8);
+// console.log(a9);
+// console.log(a10);
+// console.log(a11);
+// console.log(a12);
+// console.log(a13);
+
+// alert(a1);
+// alert(a2);
+// alert(a3);
+// alert(a4);
+// alert(a5);
+// alert(a6);
+// alert(a7);
+// alert(a8);
+// alert(a9);
+// alert(a10);
+// alert(a11);
+// alert(a12);
+// alert(a13);
+
+// document.write(a1);
+// document.write(a2);
+// document.write(a3);
+// document.write(a4);
+// document.write(a5);
+// document.write(a6);
+// document.write(a7);
+// document.write(a8);
+// document.write(a9);
+// document.write(a10);
+// document.write(a11);
+// document.write(a12);
+// document.write(a13);
+
+
+// - переопределить каждую переменную из задания 1 дав им произвольные значения Вывести каждую при помощи console.log , alert, document.write
+
+// a1 = `random`;
+// a2 = `lol`;
+// a3 = `mda`;
+// a4 = `12324`;
+// a5 = 431;
+// a6 = 34567;
+// a7 = -6.7;
+// a8 = -99999;
+// a9 = 14.3;
+// a10 = 4321;
+// a11 = false;
+// a12 = true;
+
+
+// console.log(a2);
+// console.log(a3);
+// console.log(a4);
+// console.log(a5);
+// console.log(a6);
+// console.log(a7);
+// console.log(a8);
+// console.log(a9);
+// console.log(a10);
+// console.log(a11);
+// console.log(a12);
+// console.log(a13);
+
+// alert(a1);
+// alert(a2);
+// alert(a3);
+// alert(a4);
+// alert(a5);
+// alert(a6);
+// alert(a7);
+// alert(a8);
+// alert(a9);
+// alert(a10);
+// alert(a11);
+// alert(a12);
+// alert(a13);
+
+// document.write(a1);
+// document.write(a2);
+// document.write(a3);
+// document.write(a4);
+// document.write(a5);
+// document.write(a6);
+// document.write(a7);
+// document.write(a8);
+// document.write(a9);
+// document.write(a10);
+// document.write(a11);
+// document.write(a12);
+// document.write(a13);
+
+
+// - Создать 3 числовых и 3 стринговых константы. Вывести каждую при помощи console.log , alert, document.write
+
+// const c1 = 3.14;
+// const c2 = 3;
+// const c3 = 2.4;
+
+// console.log(c1);
+// console.log(c2);
+// console.log(c3);
+
+// alert(c1);
+// alert(c2);
+// alert(c3);
+
+// document.write(c1);
+// document.write(c2);
+// document.write(c3);
+
+// - при помощи 3х разных prompt() получить 3 слова которые являются вашими ФИО. Для фамилии имени и отчества создать разные переменные. Вывести каждую при помощи console.log , alert, document.write
+
+name = prompt(`Name`);
+fullName = +prompt(`fullName`);
+surName = prompt(`Surname`);
+
+console.log(name);
+console.log(fullName);
+console.log(surName);
+
+alert(name);
+alert(fullName);
+alert(surName);
+
+document.write(name);
+document.write(fullName);
+document.write(surName);
+
+// - Взять переменные из задания 4 и сконкатенировать их в одной переменной person.
+
+let person;
