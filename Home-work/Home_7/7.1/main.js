278642fe1eb3bffdd9041eceb3bfed4a6d31a50b
Nazar Havronskyi <nazar.gavronsky.mail@gmail.com>
2020-11-09 22:23:40 +0200

    0000

---
+++ Home-work/Home_7/7.1/main.js
@@ -0,0 +1,9 @@
+let clearArr = [];
+let arr = [2, 17, 13, 6, 22, 31, 45, 66, 100, -18, [1, 2, 3, 4], 61253213, [5, 3, 2, 4], false, `qw`];
+for (let j = 0; j < arr.length; j++) {
+    const arrElement = arr[j];
+    if (typeof arrElement === `number` || typeof arrElement === `object`) {
+            clearArr.push(arrElement)
+    }
+}
+console.log(clearArr);
