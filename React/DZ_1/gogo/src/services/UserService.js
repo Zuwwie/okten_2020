77226ed586b713a46d8ecfd8aad1df58df5b38ff
Nazar Havronskyi <nazar.gavronsky.mail@gmail.com>
2020-12-15 21:37:11 +0200

    1

--- React/DZ_1/gogo/src/services/UserService.js
+++ React/DZ_1/gogo/src/services/UserService.js
@@ -2,9 +2,7 @@ export default class UserService {
   url = 'https://jsonplaceholder.typicode.com/users';
 
   async users() {
-    console.log(22);
-    
-    return await fetch('https://jsonplaceholder.typicode.com/users').then(value => value.json);
+    return await fetch('https://jsonplaceholder.typicode.com/users').then(value => value.json());
   }
   // async user(id) {
   //     return await fetch(this.url + `/${id}`).then(value=> value.json);
